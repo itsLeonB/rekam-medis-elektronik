3de37ba8a789e78fbc4572371d74bc134698166e
MUHAMMAD ARMANDO NUR RIZQY ANSAR <armandorizqy2326@gmail.com>
2023-09-22 11:04:56 +0700

    Layout

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,7 +1,12 @@
 <template>
     <button
-        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-    >
+        class="inline-flex justify-center px-4 py-2
+        border border-transparent rounded-lg
+        font-bold text-sm text-white tracking-widest
+        bg-vegan-300
+        hover:bg-vegan-400
+        focus:bg-vegan-400 focus:outline-none focus:ring-2 focus:ring-vegan-300 focus:ring-offset-2
+        active:bg-vegan-500 transition ease-in-out duration-150">
         <slot />
     </button>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
 });
 
