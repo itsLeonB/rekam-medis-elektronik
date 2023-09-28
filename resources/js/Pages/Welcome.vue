2ed63756d85e06612bd46da4cb0873f09e9a6e92
MUHAMMAD ARMANDO NUR RIZQY ANSAR <armandorizqy2326@gmail.com>
2023-09-28 20:46:37 +0700

    update view

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -14,26 +14,22 @@ defineProps({
 
 <template>
     <GuestLayout>
-        <div class="flex flex-col md:flex-row md:border md:rounded-3xl md:shadow-lg bg-white px-6 ">
+        <div class="flex flex-col lg:flex-row lg:border lg:rounded-3xl lg:shadow-lg bg-white px-6 ">
 
-            <div class="w-full max-w-lg">
-                <img src="storage/images/welcome-doctor.png" alt="">
-            </div>
+            <img src="storage/images/welcome-doctor.png" class="w-full block pt-5 max-w-lg" alt="">
 
-            <div class="flex flex-col w-full max-w-lg flex flex-row justify-center px-5">
-                <h1 class="font-bold text-2xl text-center mb-5">Hai, Selamat Datang!</h1>
+            <div class="w-full max-w-lg lg:w-96 flex flex-col justify-center px-10">
+                <h1 class="font-bold text-2xl text-center mb-5 text-zinc-700">Hai, Selamat Datang!</h1>
                 <Link :href="route('login')" as="button"
-                    class="mb-3 block justify-center px-4 py-2 border border-transparent rounded-lg font-semibold text-sm text-white tracking-widest bg-vegan-300 hover:bg-vegan-400 focus:bg-vegan-400 focus:outline-none focus:ring-2 focus:ring-vegan-400 focus:ring-offset-2 active:bg-vegan-500 transition ease-in-out duration-150">
+                    class="mx-auto mb-3 w-full max-w-[284px] block justify-center px-4 py-2 border border-transparent rounded-lg font-semibold text-sm text-white tracking-widest bg-vegan-300 hover:bg-vegan-400 focus:bg-vegan-400 focus:outline-none focus:ring-2 focus:ring-vegan-400 focus:ring-offset-2 active:bg-vegan-500 transition ease-in-out duration-150">
                 Login
                 </Link>
-                <div>
-                    <p class="text-center text-sm font-light text-zinc-700">
-                        Belum punya akun?
-                        <span class="font-semibold text-vegan-300">
-                            Klik disini
-                        </span>
-                    </p>
-                </div>
+                <p class="text-center text-sm font-light text-zinc-700">
+                    Belum punya akun?
+                    <Link class="inline-block font-semibold text-vegan-300">
+                    Klik disini
+                    </Link>
+                </p>
             </div>
         </div>
     </GuestLayout>
