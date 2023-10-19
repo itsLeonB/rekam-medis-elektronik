2376749aed345c970e889597d8a37343bc9f0165
Ellion Blessan <ellionblessan@gmail.com>
2023-10-19 11:52:31 +0700

    Revert "Created ImagingStudy migrations and models, update Condition table migration"
    This reverts commit 9a6c596d20cf5612e93198b6e9402838f2fcf487.

--- database/migrations/2023_10_16_082841_create_condition_table.php
+++ database/migrations/2023_10_16_082841_create_condition_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('resource_id');
             $table->index('resource_id');
             $table->foreign('resource_id')->references('id')->on('resource')->onDelete('cascade');
-            $table->enum('clinical_status', ['active', 'recurrence', 'relapse', 'inactive', 'remission', 'resolved'])->nullable();
+            $table->enum('clinical_status', ['active', 'recurrnce', 'relapse', 'inactive', 'remission', 'resolved'])->nullable();
             $table->enum('verification_status', ['unconfirmed', 'provisional', 'differential', 'confirmed', 'refuted', 'entered-in-error'])->nullable();
             $table->enum('severity', [24484000, 6736007, 255604002])->nullable();
             $table->string('code');
