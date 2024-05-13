6d6462f4b24e730bfed417fb3fc21c71b0b660a5
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2024-05-13 11:23:22 +0700

    udpet

--- app/Http/Controllers/ServicePriceController.php
+++ app/Http/Controllers/ServicePriceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ServicePrice;
 use Illuminate\Http\Request;
 
 class ServicePriceController extends Controller
 {
     public function index()
     {
+        return response()->json(ServicePrice::all(), 200);
     }
 
     public function store()
--- database/migrations/2024_05_13_110541_create_service_prices_table.php
+++ database/migrations/2024_05_13_110541_create_service_prices_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('servicecost', function (Blueprint $table) {
+        Schema::create('service_prices', function (Blueprint $table) {
             $table->id();
             $table->index('id');
             $table->timestamps();
