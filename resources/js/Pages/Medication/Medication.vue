bd0322776552f75a28f9347bb557ec7e8c81e48d
Daffa Daris Mahendra Ansori <5026201122@mahasiswa.integra.its.ac.id>
2024-07-09 01:17:42 +0700

    update button

--- resources/js/Pages/Medication/Medication.vue
+++ resources/js/Pages/Medication/Medication.vue
@@ -11,7 +11,8 @@
             </span>
             <p class="mb-3 text-base font-normal text-neutral-grey-100">Halaman Apoteker untuk mengelola obat.
             </p>
-            <Link :href="route('medication')" as="button"
+            <div class="button-group">
+                <Link :href="route('medication')" as="button"
                 class="mr-2 inline-flex mb-3 justify-center px-4 py-2 border border-transparent rounded-xl font-semibold text-sm teal-button text-original-white-0 transition ease-in-out duration-150 hover:shadow-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                 class="w-5 h-5 mr-2">
@@ -29,15 +30,14 @@
             </Link>
             <Link :href="route('medication.prescription')" as="button"
                 class="mr-2 inline-flex mb-3 justify-center px-4 py-2 border border-transparent rounded-xl font-semibold text-sm teal-button text-original-white-0 transition ease-in-out duration-150 hover:shadow-lg">
-            <svg  class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor" stroke="currentColor">
+            <svg  class="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="currentColor" stroke="currentColor">
                 <path
                     d="m678-134 46-46-64-64-46 46q-14 14-14 32t14 32q14 14 32 14t32-14Zm102-102 46-46q14-14 14-32t-14-32q-14-14-32-14t-32 14l-46 46 64 64ZM735-77q-37 37-89 37t-89-37q-37-37-37-89t37-89l148-148q37-37 89-37t89 37q37 37 37 89t-37 89L735-77ZM200-200v-560 560Zm0 80q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v245q-20-5-40-5t-40 3v-243H200v560h243q-3 20-3 40t5 40H200Zm280-670q13 0 21.5-8.5T510-820q0-13-8.5-21.5T480-850q-13 0-21.5 8.5T450-820q0 13 8.5 21.5T480-790ZM280-600v-80h400v80H280Zm0 160v-80h400v34q-8 5-15.5 11.5T649-460l-20 20H280Zm0 160v-80h269l-49 49q-8 8-14.5 15.5T474-280H280Z" />
             </svg>
             Permintaan resep
             </Link>
-            <div class="row">
-                <span class="font-semibold">Note : </span> <p id="check" class="text-sm">{{ message }}</p>
             </div>
+           
         </div>
         <div
             class="bg-original-white-0 overflow-hidden shadow rounded-xl md:rounded-2xl mb-8 p-6 md:py-8 md:pl-10 md:pr-14">
@@ -334,5 +334,9 @@ onMounted(() => {
     margin-left: 5px;
     position: absolute;
     right: 0;
+}
+
+.button-group {
+    display: flex;
 }
 </style>
