d52d78a3c7fd3e0302558f6dbde46466ddedc626
putriisabellaa <putriisabella1007@gmail.com>
2024-07-09 20:22:37 +0630

    update: database config

--- config/database.php
+++ config/database.php
@@ -38,6 +38,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_URI', '<connection string>'),
+            'database' => env('DB_DATABASE', 'rme'),
         ],
 
         // 'sqlite' => [
